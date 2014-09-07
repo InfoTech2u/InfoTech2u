18fc335147067f6a8a66a3c4e61fc7f2c84f7c9b
umbertomif <umberto_mif@yahoo.com>
2014-09-07 20:15:34 -0300

    beto paginas

---
+++ InfoTech2u.Verithus/InfoTech2u.Verithus.WEB/Modulos/RecursosHumanos/ManterAdmissao.aspx.cs
@@ -0,0 +1,101 @@
+﻿using InfoTech2u.Verithus.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using InfoTech2u.Verithus.VO;
+using InfoTech2u.Verithus.BS;
+
+namespace InfoTech2u.Verithus.WEB.Modulos.RecursosHumanos
+{
+    public partial class ManterAdmissao : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CarregarIncludes();
+            CarregarTarefa();
+            CarregarCargo();
+            CarregarSecao();
+        }
+
+        private void CarregarSecao()
+        {
+            TipoSecaoBS retornoSecaoBS = new TipoSecaoBS();
+            TipoSecaoVO objEntrada = new TipoSecaoVO();
+            List<TipoSecaoVO> listaSecaoVO = new List<TipoSecaoVO>();
+
+            listaSecaoVO = retornoSecaoBS.SelecionarSecaoLista(objEntrada);
+
+            this.ddlSecao.DataSource = listaSecaoVO;
+            this.ddlSecao.DataValueField = "CodigoTipoSecao";
+            this.ddlSecao.DataTextField = "Descricao";
+            this.ddlSecao.DataBind();
+            this.ddlSecao.Items.Insert(0, new ListItem("Selecionar", "0"));
+        }
+
+        private void CarregarCargo()
+        {
+            TipoCargoBS retornoCargoBS = new TipoCargoBS();
+            TipoCargoVO objEntrada = new TipoCargoVO();
+            List<TipoCargoVO> listaCargoVO = new List<TipoCargoVO>();
+
+            listaCargoVO = retornoCargoBS.SelecionarCargoLista(objEntrada);
+
+            this.ddlCargo.DataSource = listaCargoVO;
+            this.ddlCargo.DataValueField = "CodigoTipoCargo";
+            this.ddlCargo.DataTextField = "Descricao";
+            this.ddlCargo.DataBind();
+            this.ddlCargo.Items.Insert(0, new ListItem("Selecionar", "0"));
+        }
+
+        private void CarregarTarefa()
+        {
+            TipoTarefaBS retornoTarefaBS = new TipoTarefaBS();
+            TipoTarefaVO objEntrada = new TipoTarefaVO();
+            List<TipoTarefaVO> listaTarefaVO = new List<TipoTarefaVO>();
+
+            listaTarefaVO = retornoTarefaBS.SelecionarTarefaLista(objEntrada);
+
+            this.ddlTarefa.DataSource = listaTarefaVO;
+            this.ddlTarefa.DataValueField = "CodigoTipoTarefa";
+            this.ddlTarefa.DataTextField = "Descricao";
+            this.ddlTarefa.DataBind();
+            this.ddlTarefa.Items.Insert(0, new ListItem("Selecionar", "0"));
+        }
+
+        protected void CarregarIncludes()
+        {
+            string pachCss = "../../css/";
+            string pachJs = "../../js/";
+
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "css", pachCss, "style.default.css");
+
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "css", pachCss, "bootstrap-fileupload.min.css");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "css", pachCss, "bootstrap-timepicker.min.css");
+
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery-1.9.1.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery-migrate-1.1.1.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery-ui-1.9.2.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "bootstrap.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "bootstrap-fileupload.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "bootstrap-timepicker.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery.uniform.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery.validate.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery.tagsinput.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery.autogrow-textarea.js");
+            //InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "charCount.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "colorpicker.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "ui.spinner.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "chosen.jquery.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery.cookie.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "modernizr.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery.smartWizard.min.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery.maskedinput-1.3.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "jquery.maskMoney.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "custom.js");
+            InfoTech2uControlHtmlUtil.IncludeHtmlGenericControl(this.Page, "js", pachJs, "CadastroAdmissaoDemissao.js");
+        }
+    }
+}
---
+++ InfoTech2u.Verithus/InfoTech2u.Verithus.WEB/Modulos/RecursosHumanos/ManterCargo.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Modulos/InfoTech2u.Master" AutoEventWireup="true" CodeBehind="ManterCargo.aspx.cs" Inherits="InfoTech2u.Verithus.WEB.Modulos.RecursosHumanos.ManterCargo" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolderInfoTech2u" runat="server">
+    <div class="rightpanel">
+
+        <ul class="breadcrumbs">
+            <li><a href="dashboard.html"><i class="iconfa-home"></i></a><span class="separator"></span></li>
+            <li><a href="forms.html">Forms</a> <span class="separator"></span></li>
+            <li>Form Styles</li>
+
+        </ul>
+
+        <div class="pageheader">
+            <div class="pageicon"><span class="iconfa-pencil"></span></div>
+            <div class="pagetitle">
+                <h5>Cargo
+                </h5>
+                <h1>Fomulário de Cargo</h1>
+            </div>
+        </div>
+        <!--pageheader-->
+
+        <div class="maincontent">
+            <div class="maincontentinner">
+                <div class="widgetbox box-inverse">
+                    <h4 class="widgettitle">Dados Cargo</h4>
+                    <div class="widgetcontent nopadding">
+                        <div class="stdform stdform2">
+                          
+                            <p id="validaDescricao">
+                                <label>Descrição</label>
+                                <span class="field">
+                                    <asp:TextBox runat="server" ID="txtDescricao" class="input-small" ClientIDMode="Static" />
+                                    <span class="help-inline" id="msgDescricao"></span>
+                                </span>
+                            </p>
+
+
+                            <p class="stdformbutton">
+                                <a href="#"  id="btnConcluir"  class="btn btn-primary btn-rounded">Concluir</a>
+                                <a href="#" id="btnLimpar" class="btn btn-rounded"><i class="iconfa-refresh iconsweets-black"></i>&nbsp; limpar</a>
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <!--widget-->
+
+                <div class="footer">
+                    <div class="footer-left">
+                        <span>&copy; 2013. Shamcey Admin Template. All Rights Reserved.</span>
+                    </div>
+                    <div class="footer-right">
+                        <span>Designed by: <a href="http://themepixels.com/">ThemePixels</a></span>
+                    </div>
+                </div>
+                <!--footer-->
+
+            </div>
+            <!--maincontentinner-->
+        </div>
+        <!--maincontent-->
+
+    </div>
+    <!--rightpanel-->
+</asp:Content>
