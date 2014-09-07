445fbed1c16d1322b81d31117f7cdaa55e7169a2
umbertomif <umberto_mif@yahoo.com>
2014-09-07 17:35:12 -0300

    beto

--- InfoTech2u.Verithus/InfoTech2u.Verithus.BS/TipoTarefaBS.cs
+++ InfoTech2u.Verithus/InfoTech2u.Verithus.BS/TipoTarefaBS.cs
@@ -10,5 +10,22 @@ namespace InfoTech2u.Verithus.BS
 {
     public class TipoTarefaBS
     {
+        public List<TipoTarefaVO> SelecionarTarefaLista(TipoTarefaVO objEntrada)
+        {
+            TipoTarefaDA objRetorno = new TipoTarefaDA();
+
+            try
+            {
+                return objRetorno.SelecionarTarefaLista(objEntrada);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                objRetorno = null;
+            }
+        }
     }
 }
