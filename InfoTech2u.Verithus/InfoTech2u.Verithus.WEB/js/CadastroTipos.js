a4cdd20044626d4101d880fb79ea6f99367a089e
umbertomif <umberto_mif@yahoo.com>
2014-09-07 17:29:08 -0300

    beto

---
+++ InfoTech2u.Verithus/InfoTech2u.Verithus.WEB/js/CadastroTipos.js
@@ -0,0 +1,30 @@
+﻿
+
+jQuery(document).ready(function () {
+
+    jQuery("#btnConcluir").click(function () {
+        ValidarFormulario();
+    });
+
+    jQuery("#btnLimpar").click(function () {
+        jQuery('#txtDescricao').val("");
+        jQuery('#msgDescricao').html('').hide();
+        jQuery("#validaDescricao").removeClass("par control-group error").addClass("input-small");
+    });
+
+    function ValidarFormulario() {
+
+        var descricao = jQuery('#txtDescricao').val();
+        if (!descricao && descricao.length <= 0) {
+            jQuery('#msgDescricao').html('O Campo Descrição Deve ser preenchido').show();
+            jQuery("#validaDescricao").removeClass("par control-group success").addClass("par control-group error");
+            retorno = false;
+        }
+        else {
+            jQuery('#msgDescricao').html('').hide();
+            jQuery("#validaDescricao").removeClass("par control-group error").addClass("par control-group success");
+        }
+
+    }
+
+});
