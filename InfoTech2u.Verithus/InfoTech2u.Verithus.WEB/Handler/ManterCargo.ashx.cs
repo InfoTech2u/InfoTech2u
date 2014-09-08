134c48fab265a190e4bc443f2f27a2cd2e3bd0a6
umbertomif <umberto_mif@yahoo.com>
2014-09-08 15:32:21 -0300

    beto

---
+++ InfoTech2u.Verithus/InfoTech2u.Verithus.WEB/Handler/ManterCargo.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfoTech2u.Verithus.WEB.Handler
+{
+    /// <summary>
+    /// Summary description for ManterCargo
+    /// </summary>
+    public class ManterCargo : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
