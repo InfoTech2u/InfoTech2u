c3b7db72c7d6fd04f2d80638d865c9d7c0b4bddf
umbertomif <umberto_mif@yahoo.com>
2014-09-14 21:55:39 -0300

    beto

--- InfoTech2u.Verithus/InfoTech2u.Verithus.DA/TipoCargoDA.cs
+++ InfoTech2u.Verithus/InfoTech2u.Verithus.DA/TipoCargoDA.cs
@@ -62,12 +62,12 @@ namespace InfoTech2u.Verithus.DA
             }
         }
 
-        public bool IncluirTipoCargo(TipoCargoVO param)
+        public DataTable IncluirTipoCargo(TipoCargoVO param)
         {
             InfoTech2uSQLUtil objSql = null;
             StringBuilder query = null;
             List<SqlParameter> lstSqlParameter = null;
-            DataTable dt = null;
+            DataTable dtRetorno = null;
 
             try
             {
@@ -87,22 +87,17 @@ namespace InfoTech2u.Verithus.DA
                 lstSqlParameter.Add(new SqlParameter("@CODIGO_ALTERACAO", param.CodigoAlteracao));
                 lstSqlParameter.Add(new SqlParameter("@CODIGO_STATUS", param.CodigoStatus));
 
-                lstSqlParameter.Add(new SqlParameter("@C_ERR", DBNull.Value));
-                lstSqlParameter.Add(new SqlParameter("@T_ERR", DBNull.Value));
+                dtRetorno = new DataTable();
 
-                dt = new DataTable();
+                objSql.Execute("dbo.[SPVRT078_TIPO_CARGO_PR_INCLUIR]", lstSqlParameter.ToArray(), null, ref dtRetorno);
 
-                int rowsAffected = 0;
-                objSql.ExecuteNonQuery("dbo.[SPVRT078_TIPO_CARGO_PR_INCLUIR]", lstSqlParameter.ToArray(), null, out rowsAffected);
-
-                return rowsAffected > 0;
             }
             catch (Exception ex)
             {
-                return false;
+                return null;
             }
 
-            return true;
+            return dtRetorno;
         }
         public bool ExcluirTipoCargo(TipoCargoVO param)
         {
@@ -123,6 +118,8 @@ namespace InfoTech2u.Verithus.DA
                 objSql.Open();
 
                 lstSqlParameter.Add(new SqlParameter("@CODIGO_TIPO_CARGO", param.CodigoTipoCargo));
+                lstSqlParameter.Add(new SqlParameter("@C_ERR", DBNull.Value));
+                lstSqlParameter.Add(new SqlParameter("@T_ERR", DBNull.Value));
 
                 int rowsAffected = 0;
 
