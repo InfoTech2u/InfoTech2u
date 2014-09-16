3cc625f0f6a9b4935f5412d352a1d85f12c78e69
rubens-senday <rubenssenday@yahoo.com.br>
2014-09-15 22:38:06 -0300

    Rubens

---
+++ InfoTech2u.Verithus/InfoTech2u.Verithus.WEB/Handler/ManterContribuicaoSindical.ashx.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InfoTech2u.Verithus.Util;
+using InfoTech2u.Verithus.VO;
+using System.Data;
+using InfoTech2u.Verithus.BS;
+
+namespace InfoTech2u.Verithus.WEB.Handler
+{
+    /// <summary>
+    /// Summary description for ManterContribuicaoSindical
+    /// </summary>
+    public class ManterContribuicaoSindical : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.Request.QueryString["Metodo"] == "SelecionarContribuicaoFuncionario")
+            {
+                var retorno = SelecionarContribuicaoFuncionario(Convert.ToInt32(context.Request.QueryString["CodigoFuncionario"]));                
+
+                context.Response.Write(retorno.DataTableSerializer());
+            }
+            else if (context.Request.QueryString["Metodo"] == "Incluir")
+            {
+                context.Response.Write(IncluirContribuicaoSindical(context).DataTableSerializer());
+            }
+            else if (context.Request.QueryString["Metodo"] == "Alterar")
+            {
+                context.Response.Write(AlterarContribuicaoSindical(context).DataTableSerializer());
+            }
+            else if (context.Request.QueryString["Metodo"] == "Excluir")
+            {
+                context.Response.Write(ExcluirContribuicaoSindical(context));
+            }
+        }
+
+        private DataTable SelecionarContribuicaoFuncionario(int idFuncionario)
+        {
+            ContribuicaoSindicalBS objBS = null;
+            ContribuicaoSindicalVO contribuicao = null;
+            try
+            {
+                objBS = new ContribuicaoSindicalBS();
+                contribuicao = new ContribuicaoSindicalVO();
+                contribuicao.CodigoFuncionario = idFuncionario;
+
+                return objBS.SelecionarContribuicaoFuncionario(contribuicao);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                objBS = null;
+                contribuicao = null;
+            }
+        }
+
+        private DataTable IncluirContribuicaoSindical(HttpContext context)
+        {
+            ContribuicaoSindicalBS objBS = null;
+            ContribuicaoSindicalVO contribuicao = null;
+
+            try
+            {
+                objBS = new ContribuicaoSindicalBS();
+                contribuicao = new ContribuicaoSindicalVO();
+
+                contribuicao.CodigoFuncionario = Convert.ToInt32(context.Request.QueryString["CodigoFuncionario"]);
+                contribuicao.PeriodoAno = Convert.ToDateTime(context.Request.QueryString["PeriodoAno"]);
+                contribuicao.CodigoSindicato = Convert.ToInt32(context.Request.QueryString["CodigoSindicato"]);
+                contribuicao.ValorRecolhido = context.Request.QueryString["ValorRecolhido"];
+                contribuicao.CodigoStatus = null;
+                contribuicao.CodigoUsuarioAlteracao = null;
+                contribuicao.CodigoUsuarioCadastro = null;
+                contribuicao.DataAlteracao = null;
+                contribuicao.DataCadastro = null;
+
+                return objBS.IncluirContribuicaoSindical(contribuicao);
+                
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+            finally
+            {
+                objBS = null;
+                contribuicao = null;
+            }
+        }
+
+        public DataTable AlterarContribuicaoSindical(HttpContext context)
+        {
+            ContribuicaoSindicalBS objBS = null;
+            ContribuicaoSindicalVO contribuicao = null;
+
+            try
+            {
+                objBS = new ContribuicaoSindicalBS();
+                contribuicao = new ContribuicaoSindicalVO();
+
+                contribuicao.CodigoContribuicaoSindical = Convert.ToInt32(context.Request.QueryString["CodigoContribuicaoSindical"]);
+                contribuicao.PeriodoAno = Convert.ToDateTime(context.Request.QueryString["PeriodoAno"]);
+                contribuicao.CodigoSindicato = Convert.ToInt32(context.Request.QueryString["CodigoSindicato"]);
+                contribuicao.ValorRecolhido = context.Request.QueryString["ValorRecolhido"];
+                contribuicao.CodigoStatus = null;
+                contribuicao.CodigoUsuarioAlteracao = null;
+                contribuicao.DataAlteracao = DateTime.Now;
+
+                return objBS.AlterarContribuicaoSindical(contribuicao);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                objBS = null;
+                contribuicao = null;
+            }
+        }
+
+        private bool ExcluirContribuicaoSindical(HttpContext context)
+        {
+            ContribuicaoSindicalBS objBS = null;
+            ContribuicaoSindicalVO contribuicao = null;
+
+            try
+            {
+                objBS = new ContribuicaoSindicalBS();
+                contribuicao = new ContribuicaoSindicalVO();
+
+                contribuicao.CodigoContribuicaoSindical = Convert.ToInt32(context.Request.QueryString["CodigoContribuicaoSindical"]);
+
+                return objBS.ExcluirContribuicaoSindical(contribuicao);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                objBS = null;
+                contribuicao = null;
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
