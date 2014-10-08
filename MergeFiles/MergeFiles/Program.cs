c90d728684c693b3ecf9f9ddf805d9f0d358c7c0
rubens-senday <rubenssenday@yahoo.com.br>
2014-10-08 08:45:27 -0300

    Merge FIles
    Projeto para unir todas os scripts de criação de procedures em um só
    arquivo.

---
+++ MergeFiles/MergeFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int chunkSize = 2 * 1024; // 2KB
+            var path = @"E:\Projetos\InfoTech2u - Copia\Verithus\Codificação\Banco de Dados\Procedures";
+
+
+            var inputFiles = Directory.GetFiles(path);
+
+
+            var output = @"E:\Projetos\InfoTech2u - Copia\Verithus\Codificação\Banco de Dados\Procedures\ProceduresCreation.sql";
+
+            foreach (var file in inputFiles)
+            {
+                var input = File.ReadAllText(file);
+                File.AppendAllText(output, "GO" + Environment.NewLine);
+                File.AppendAllText(output, input + Environment.NewLine);
+            }
+            
+        }
+    }
+}
