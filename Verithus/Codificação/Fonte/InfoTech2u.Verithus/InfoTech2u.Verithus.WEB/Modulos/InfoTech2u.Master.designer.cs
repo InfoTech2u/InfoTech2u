6b45d4513257cece4b129a1085e1165b3c8c812e
InfoTech2u <romulo.ferreira@infotech2u.com.br>
2014-10-12 22:21:35 -0300

    Remoção Pesquisa Empresa

--- Verithus/Codificação/Fonte/InfoTech2u.Verithus/InfoTech2u.Verithus.WEB/Modulos/InfoTech2u.Master.designer.cs
+++ Verithus/Codificação/Fonte/InfoTech2u.Verithus/InfoTech2u.Verithus.WEB/Modulos/InfoTech2u.Master.designer.cs
@@ -76,15 +76,6 @@ namespace InfoTech2u.Verithus.WEB {
         protected global::System.Web.UI.WebControls.HyperLink hlkManterUsuario;
         
         /// <summary>
-        /// hlkPesquisaEmpresa control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hlkPesquisaEmpresa;
-        
-        /// <summary>
         /// hlkManterEmpresa control.
         /// </summary>
         /// <remarks>
